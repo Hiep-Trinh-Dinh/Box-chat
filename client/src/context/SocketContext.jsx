d5b217818910dd45dc0df937620800acc577940c
Enryu <you@example.com>
2024-07-24 16:14:25 +0700

    Socket url updated three

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://box-chat.onrender.com", {
+			const socket = io("https://box-chat.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
